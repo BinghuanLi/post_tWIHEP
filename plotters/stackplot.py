202a593842929d6864cb79e50fbb0fc7755d7ec7
binghuan li <binghuan.li@cern.ch>
2019-03-05 01:43:35 +0800

    add new plotter for output of mvaTool.C

--- plotters/stackplot.py
+++ plotters/stackplot.py
@@ -14,15 +14,15 @@
 #
 #
 # ***
-# This program is modified to produce post-fit/pre-fit StackPlots and Data/MC comparison and the yield table
-# The script takes the output of CombineTool
-# python ratioplot.py -f <fittype> -r <region> -p <POI> -o <plotOpen> -d<dir> -l<LATEX> -b<blind> -c<category> -s<SPLIT>
-# python ratioplot.py -f prefit -r 2lss -p Bin2l -l 1 -b 1 -c SubCat2l -s ee_neg
-# 12 Sep 2018   Binghuan Li
+# This program is modified to produce pre-fit StackPlots and Data/MC comparison and the yield table
+# The script takes the output of mvaTool.C
+# python stackplot.py -r <region> -p <POI> -o <plotOpen> -d<dir> -l<LATEX> -b<blind> -c<category> -s<SPLIT> 
+# python stackplot.py -r 2lss -p Bin2l -l 1 -b 1 -c SubCat2l
+# 4 March 2019   Binghuan Li
 # *** 
 #
 
-import os
+import sys, os, subprocess
 import optparse
 import distutils.util
 import math
@@ -32,20 +32,18 @@ from ROOT import kBlack, kBlue, kRed, kCyan, kViolet, kGreen, kOrange, kGray, kP
 
 usage = 'usage: %prog [options]'
 parser = optparse.OptionParser(usage)
-parser.add_option('-f', '--fit',        dest='fit_type'  ,      help='prefit/fit_b or fit_s',      default='prefit',        type='string')
 parser.add_option('-r', '--region',        dest='region'  ,      help='region to plot: 2lss or ttWctrl',      default='2lss',        type='string')
 parser.add_option('-b', '--blind',        dest='blind'  ,      help='to plot data(0) or not(1)',      default='1',        type='int')
 parser.add_option('-p', '--parameter',        dest='POI'  ,      help='parameter of interest',      default='Bin2l',        type='string')
 parser.add_option('-c', '--category',        dest='SubCat'  ,      help='categorization',      default='SubCat2l',        type='string')
 parser.add_option('-o', '--open',        dest='OPEN'  ,      help='to hold plot open(1) or not(0)',      default='0',        type='int')
-parser.add_option('-d', '--dir',        dest='DirOfRootplas'  ,      help='input file full path',      default='/home/binghuan/Work/RootTestFiles/TTHLep_2019/data/2019Rootplas/rootplas_20190227/V0227_datacards/',        type='string')
+parser.add_option('-d', '--dir',        dest='DirOfRootplas'  ,      help='input file full path',      default='/home/binghuan/Work/RootTestFiles/TTHLep_2019/data/2019Rootplas/rootplas_20190227/Output/',        type='string')
 parser.add_option('-l', '--latex',        dest='LATEX'  ,      help='to print latex(1) or not(0)',      default='0',        type='int')
 parser.add_option('-s', '--split',        dest='SPLIT'  ,      help='to produce plot in subgategory',      default='inclusive',        type='string')
 
 (opt, args) = parser.parse_args()
 POI = opt.POI
 region = opt.region
-fit_type = opt.fit_type
 OPEN = opt.OPEN
 LATEX = opt.LATEX
 DirOfRootplas = opt.DirOfRootplas
@@ -54,21 +52,23 @@ SPLIT = opt.SPLIT
 blind = opt.blind
 
 #header_postfix = "2lss l^{#pm}l^{#pm} "
-header_postfix = "2lss "
+header_postfix = " 2lss "
 if region =="ttWctrl": header_postfix = " ttWctrl "
 
+Expected = "expected" 
+if blind !=1 : Expected = "observed"
 
-Samples=["TTH","H","TTW+TTWW","TTZ","EWK","Rares","Conv","Fakes","Flips"]
+Samples=["TTH","H","TTW+TTWW","TTZ","EWK","Rares","Conv","Fakes","Flips","Data"]
 Process={
     "TTH":["TTH_hww","TTH_hzz","TTH_htt","TTH_hmm","TTH_hot"],
     "H":["THW_hww","THW_hzz","THW_htt","THQ_hww","THQ_hzz","THQ_htt"],
-    "TTZ":["TTZ"],"TTW+TTWW":["TTW","TTWW"],"Conv":["Conv"],"EWK":["EWK"],"Rares":["Rares"],"Fakes":["Fakes"],"Flips":["Flips"]
+    "TTZ":["TTZ"],"TTW+TTWW":["TTW","TTWW"],"Conv":["Conv"],"EWK":["EWK"],"Rares":["Rares"],"Fakes":["Fakes","FakeSub"],"Flips":["Flips"],"Data":["Data"]
     }
 Color={"TTH":kRed,"H":kPink,"TTZ":kGreen,"TTW+TTWW":kGreen+3,"Conv":kOrange,"EWK":kViolet,"Rares":kCyan,"Fakes":kBlack,"Flips":kBlack}
 Style={"TTH":1001,"H":1001,"TTZ":1001,"TTW+TTWW":1001,"Conv":1001,"EWK":1001,"Rares":1001,"Fakes":3005,"Flips":3006}
 
 subCats={
-"SubCat2l":["ee_neg","ee_pos", "em_bl_neg","em_bl_pos","em_bt_neg","em_bt_pos", "mm_bl_neg","mm_bl_pos","mm_bt_neg","mm_bt_pos" ],
+"SubCat2l":["inclusive","ee_neg","ee_pos", "em_bl_neg","em_bl_pos","em_bt_neg","em_bt_pos", "mm_bl_neg","mm_bl_pos","mm_bt_neg","mm_bt_pos" ],
 "DNNCat":["ttHnode","ttJnode","ttWnode","ttZnode"],
 "DNNCat_option2":["ttHnode","ttJnode","ttWnode","ttZnode"],
 "DNNCat_option3":["ttHnode","ttJnode","ttWnode","ttZnode"],
@@ -76,23 +76,23 @@ subCats={
 
 Channels = []
 if not subCats.has_key(SubCat):
-    print ("ERROR : " + SubCat +"is not a key of subCats ")
-    os._exit()
-elif SPLIT=="inclusive":
-    Channels = subCats[SubCat]
-    print (" Plot all channels ")
-elif SPLIT not in subCats[SubCat]:
-    print ("ERROR : " + SPLIT +"is not channel of "+SubCat)
+    print ("ERROR : " + SubCat +" is not a key of subCats ")
     os._exit()
 else:
     Channels = [SPLIT]
     header_postfix += "," + SPLIT.replace("_","\_") 
     print (" Plot channel "+SPLIT)
 
+outputDir = DirOfRootplas+"plots/"+SubCat+"/"+region+"/"+SPLIT
+inputRegName = region
+if region == "2lss": inputRegName = "SigRegion"
 
-def read_rootfile(samplename=""):
+if not os.path.exists(outputDir):
+    os.popen("mkdir -p "+outputDir)
+
+def read_rootfile(samplename="TTH_hww"):
     ''' read a root file '''
-    fullfilename = DirOfRootplas+SubCat+"/"+POI+"/fitDiagnostics"+samplename+".root"
+    fullfilename = DirOfRootplas+SubCat+"/"+SPLIT+"/"+inputRegName+"/output_"+SPLIT+"_"+samplename+".root"
     inputfile = TFile(fullfilename,"read")
     return inputfile
 
@@ -122,20 +122,29 @@ def createSum(h1, h2):
     return h3
 
 
-def readHists(postfix=""):
+def readHists():
     hstack = THStack("hstack","hstack")
     hstack.SetTitle("#scale[0.9]{#font[22]{CMS} #font[12]{Preliminary}                                                              41.53 fb^{-1}(13TeV)}")
-    inputfile  = read_rootfile(postfix)
+    inputfile  = read_rootfile() # assuming TTH_hww is always there 
     gROOT.cd()
-    dirName = "ttH_"+region+"_"+Channels[0]
-    h0 = inputfile.Get("shapes_"+fit_type+"/"+dirName+"/total")
+    histName = POI+"_TTH_hww" 
+    h0 = inputfile.Get(histName)
     h_totalbkg = h0.Clone("h_totalbkg")
     h_totalbkg.SetDirectory(0)
     h_totalbkg.Reset()
+    h_totalbkg.SetLineColor(kBlack)
+    h_totalbkg.SetFillColor(kGray+3)
+    h_totalbkg.SetFillStyle(3001)
+    h_totalbkg.SetTitle("")
+    h_totalbkg.SetMinimum(0.8)
+    h_totalbkg.SetMaximum(1.35)
+    h_totalbkg.Sumw2()
+    h_totalbkg.SetStats(0)
             
     h_totalsig = h0.Clone("h_totalsig")
     h_totalsig.SetDirectory(0)
     h_totalsig.Reset()
+    h_totalsig.SetMarkerStyle(20)
 
     h_totalmc = h0.Clone("h_totalmc")
     h_totalmc.SetDirectory(0)
@@ -157,49 +166,12 @@ def readHists(postfix=""):
     latexString =""
     expString =" SM Exp"
     dataString =" Data Obs"
-    latexString += ("\\begin{frame}\n\\frametitle{"+POI.replace("_","\_")+" "+fit_type.replace("_","\_")+"}\n\\begin{table}[]\n\scalebox{0.38}{\n\\begin{tabular}{"+"l"*(2+len(Channels))+"}\n")
-    for channel in Channels:
-        directory = "shapes_"+fit_type+"/ttH_"+region+"_"+channel+"/" 
-        h_bkg = inputfile.Get(directory+"total_background").Clone(directory+"total_background")
-        h_totalbkg.Add(h_bkg)
-         
-        h_sig = inputfile.Get(directory+"total_signal").Clone(directory+"total_signal")
-        h_totalsig.Add(h_sig)
-    
-        h_total = inputfile.Get(directory+"total").Clone(directory+"total")
-        h_totalmc.Add(h_total)
-    
-        g_data = inputfile.Get(directory+"data").Clone(directory+"data")
-        h_data = h_total.Clone("h_data")
-        h_data.Reset()
-        TGraphToTH1(h_data, g_data)
-        h_dataobs.Add(h_data)
-       
-        
-        SM_error = ROOT.Double(0)
-        h_total.IntegralAndError(0,h_total.GetNbinsX(),SM_error)
-        expString += "  &  $ "+str(round(h_total.Integral(),2))+" \\pm "+str(round(SM_error,2))+"$"
-        
-        data_error = ROOT.Double(0)
-        h_data.IntegralAndError(0,h_data.GetNbinsX(),data_error)
-        dataString += "  &  $ "+str(round(h_data.Integral(),2))+" \\pm "+str(round(data_error,2))+"$"
-        
-         
-        latexString += ("& "+channel.replace("_","\_")+"   ")
-
-    totMC_error = ROOT.Double(0)
-    h_totalmc.IntegralAndError(0,h_totalmc.GetNbinsX(),totMC_error)
-    expString += "  &  $ "+str(round(h_totalmc.Integral(),2))+" \\pm "+str(round(totMC_error,2))+"$ \\\\ \n"
-    
-    totdata_error = ROOT.Double(0)
-    h_dataobs.IntegralAndError(0,h_dataobs.GetNbinsX(),totdata_error)
-    dataString += "  &  $ "+str(round(h_dataobs.Integral(),2))+" \\pm "+str(round(totdata_error,2))+"$ \n"
-    
-    latexString += "& total "+ region +" \\\\ \n"
+    latexString += ("\\begin{frame}\n\\frametitle{"+POI.replace("_","\_")+"}\n\\begin{table}[]\n\scalebox{0.8}{\n\\begin{tabular}{"+"l"*(1+len(Channels))+"}\n")
+    latexString += ("& "+SPLIT.replace("_","\_")+" \\\\ \n")
 
     legend = TLegend(0.2,0.6,0.8,0.88)
     #legend.SetHeader("2017 "+fit_type+", 2lss l^{#pm}l^{#pm} #mu(ttH)=#hat#mu")
-    legend.SetHeader("2017 "+fit_type+", "+header_postfix)
+    legend.SetHeader("2017 prefit, "+header_postfix)
     legend.SetNColumns(3)
     legend.SetBorderSize(0)
 
@@ -207,45 +179,89 @@ def readHists(postfix=""):
     for sample in Samples:
         hist=h_totalsig.Clone(sample)
         hist.Reset()
-        latexString += sample
-        for channel in Channels:
-            h_sample =hist.Clone(sample+"_sample")
-            h_sample.Reset()
-            latexString += "    & "
+        if not sample == "Data" : latexString += sample + " &"
+        # loop over data
+        if sample == "Data" or sample == "data":
             for p in Process[sample]:
-                histname = "shapes_"+fit_type+"/ttH_"+region+"_"+channel+"/"+p 
-                h1 = inputfile.Get(histname)
-                if h1:
-                    #print h1.Integral()
-                    hist.Add(h1)
-                    h_sample.Add(h1)
-            if h_sample.Integral()>0.05:
-                error = ROOT.Double(0)
-                h_sample.IntegralAndError(0,h_sample.GetNbinsX(),error)
-                latexString += "$ "+str(round(h_sample.Integral(),2))+" \\pm "+str(round(error,2))+"$"
-            elif h_sample.Integral()>0:
-                latexString += "  \\textless{}0.05"
-            else: latexString += " - "
-                 
-        if hist.Integral()>0: 
-            hist.SetDirectory(0)
+                rootfile  = read_rootfile(p)
+                gROOT.cd()
+                h1 = rootfile.Get(POI+"_"+p)
+                #h1.SetDirectory(0)
+                h_dataobs.Add(h1)
+                rootfile.Close()
+        # loop over mc
+        # loop over signal
+        elif "TTH" in sample:
+            for p in Process[sample]:
+                rootfile  = read_rootfile(p)
+                gROOT.cd()
+                h1 = rootfile.Get(POI+"_"+p)
+                #h1.SetDirectory(0)
+                h_totalsig.Add(h1)
+                h_totalmc.Add(h1)
+                hist.Add(h1)
+                rootfile.Close()
             hist.SetFillColor(Color[sample])
             hist.SetLineColor(kBlack)
             hist.SetFillStyle(Style[sample])
+            if hist.Integral()>0.05:
+                error = ROOT.Double(0)
+                hist.IntegralAndError(0,hist.GetNbinsX(),error)
+                latexString += "$ "+str(round(hist.Integral(),2))+" \\pm "+str(round(error,2))+"$ \\\\ \n"
+            elif hist.Integral()>0:
+                latexString += "  \\textless{}0.05 \\\\ \n"
+            else: latexString += " - \\\\ \n "
             hstack.Add(hist)
             legend.AddEntry(hist,sample,"f")
+        # loop over bkg
+        else:
+            for p in Process[sample]:
+                rootfile  = read_rootfile(p)
+                gROOT.cd()
+                #print " try to get histogram : " + POI+"_"+p
+                h1 = rootfile.Get(POI+"_"+p)
+                h1.SetDirectory(0)
+                if p == "FakeSub" and sample == "Fakes":
+                    h_totalbkg.Add(h1, -1)
+                    h_totalmc.Add(h1, -1)
+                    hist.Add(h1, -1)
+                else:
+                    h_totalbkg.Add(h1)
+                    h_totalmc.Add(h1)
+                    hist.Add(h1)
+                rootfile.Close()
+            hist.SetFillColor(Color[sample])
+            hist.SetLineColor(kBlack)
+            hist.SetFillStyle(Style[sample])
             if hist.Integral()>0.05:
-                IntegralError = ROOT.Double(0)
-                hist.IntegralAndError(0,hist.GetNbinsX(),IntegralError)
-                latexString += "    & $ "+str(round(hist.Integral(),2))+" \\pm "+str(round(IntegralError,2))+" $ \\\\ \n"
-            else:
-                latexString += "    & \\textless{}0.05 \\\\ \n"
-        else: latexString += " & - \\\\ \n"
+                error = ROOT.Double(0)
+                hist.IntegralAndError(0,hist.GetNbinsX(),error)
+                latexString += "$ "+str(round(hist.Integral(),2))+" \\pm "+str(round(error,2))+"$ \\\\ \n"
+            elif hist.Integral()>0:
+                latexString += "  \\textless{}0.05 \\\\ \n"
+            else: latexString += " - "
+            hstack.Add(hist)
+            legend.AddEntry(hist,sample,"f")
+                 
+        
 
-    latexString += (expString + dataString)
+    totMC_error = ROOT.Double(0)
+    h_totalmc.IntegralAndError(0,h_totalmc.GetNbinsX(),totMC_error)
+    expString += "  &  $ "+str(round(h_totalmc.Integral(),2))+" \\pm "+str(round(totMC_error,2))+"$ \\\\ \n"
+    
+    totbkg_error = ROOT.Double(0)
+    h_totalbkg.IntegralAndError(0,h_totalbkg.GetNbinsX(),totbkg_error)
+    expString += "Tot Bkg  &  $ "+str(round(h_totalbkg.Integral(),2))+" \\pm "+str(round(totbkg_error,2))+"$ \\\\ \n"
+    
+    totdata_error = ROOT.Double(0)
+    h_dataobs.IntegralAndError(0,h_dataobs.GetNbinsX(),totdata_error)
+    dataString += "  &  $ "+str(round(h_dataobs.Integral(),2))+" \\pm "+str(round(totdata_error,2))+"$ \n"
+    
+    if not blind == 1: latexString += (expString + dataString)
+    else : latexString += expString
     latexString += ("\\end{tabular}\n}\n\\end{table}\n\\end{frame}\n \\end{document}\n")
     if LATEX==1 : 
-        latexfile = file(DirOfRootplas+SubCat+"/"+POI+"/"+fit_type+"_"+SPLIT+"_yield.tex","w")
+        latexfile = file(outputDir+"/"+POI+"_"+Expected+"_yield.tex","w")
         latexfile.write("\\documentclass{beamer}\n\\usetheme{Warsaw}\n\n\\usepackage{graphicx}\n\\useoutertheme{infolines}\n\\setbeamertemplate{headline}{}\n\n\\begin{document}\n\n")
         latexfile.write(latexString)
 
@@ -267,7 +283,10 @@ def createTotalMCErr(h1, POI):
 
     # Adjust y-axis settings
     y = h2.GetYaxis()
-    y.SetTitle("Data/pred. ")
+    if blind==1 :
+        y.SetTitle("Sig/Bkg. ")
+    else:
+        y.SetTitle("Data/pred. ")
     y.CenterTitle()
     y.SetNdivisions(505)
     y.SetTitleSize(25)
@@ -322,16 +341,19 @@ def createCanvasPads():
 
 
 
-def ratioplot():
+def stackplot():
     
     # create required parts
     h_totalsig, h_totalbkg, h_TotalMC, h_dataobs, hstack, leg = readHists()
-    h_ratio = createRatio(h_dataobs, h_TotalMC)
-    h_MCerr = createTotalMCErr(h_TotalMC, POI)
+    if blind ==1 :
+        h_MCerr = createTotalMCErr(h_totalbkg, POI)
+        h_ratio = createRatio(h_totalsig, h_totalbkg)
+    else:
+        h_MCerr = createTotalMCErr(h_TotalMC, POI)
+        h_ratio = createRatio(h_dataobs, h_TotalMC)
     c, pad1, pad2 = createCanvasPads()
-    Expected = "expected" 
-    if blind !=1 : Expected = "observed"
-    leg.AddEntry(h_dataobs,Expected,"lep")
+    if not blind ==1 :
+        leg.AddEntry(h_dataobs,"observed","lep")
     leg.AddEntry(h_TotalMC,"Uncertainty","f")
     
     # draw everything
@@ -354,7 +376,8 @@ def ratioplot():
     
     
     h_TotalMC.Draw("e2same")
-    h_dataobs.Draw("same")
+    if blind !=1:
+        h_dataobs.Draw("same")
     leg.Draw("same")
     
     
@@ -366,16 +389,20 @@ def ratioplot():
     #axis.Draw()
     
     pad2.cd()
-    h_MCerr.SetMinimum(0.5)
-    h_MCerr.SetMaximum(1.8)
+    if blind ==1 :
+        h_MCerr.SetMinimum(0.)
+        h_MCerr.SetMaximum(1.)
+    else:
+        h_MCerr.SetMinimum(0.5)
+        h_MCerr.SetMaximum(1.8)
     h_MCerr.Draw("e2") 
     h_ratio.Draw("epsame")
   
-    c.SaveAs(DirOfRootplas+SubCat+"/"+POI+"/"+fit_type+"_"+SPLIT+"_"+Expected+".png") 
+    c.SaveAs(outputDir+"/"+POI+"_"+Expected+".png") 
     # To hold window open when running from command line
     if OPEN==1: text = raw_input()
 
 # Draw all canvases 
 if __name__ == "__main__":
-    ratioplot()
+    stackplot()
 
